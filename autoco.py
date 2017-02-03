8fcce43938c8a48c7afc7c1a89a3634194e01379
Javier Herrera <muchorrollodejavi@hotmail.com>
2017-02-02 19:58:15 -0500

    settings, lista dispositivos y particiones y los diferencia

--- autoco.py
+++ autoco.py
@@ -1,5 +1,25 @@
 from subprocess import call
 import os
+import glob
+import settings
+import time
+
+disks = []
+temp_disk = []
+
+def ddcopy():
+    call(['dd','if='+file,'of='+settings.DD_SAVE_DIR+time.strftime('%S:%H-%d-%m-%Y]',time.gmtime())+'.iso'])
 
 while True:
-    os.chdir(/dev/disk/by-path)
+    os.chdir('/dev/disk/by-path') #directorio donde buscar los discos y particiones
+    for file in glob.glob('*[usb]*'): #filtro para solo mostrar los dispositivos usb
+        temp_disk.append(file)
+        if disks.count(file) == 0:
+            disks.append(file) #guardo en una lista cada uno de los archivos
+            if 'part' in file:
+                print('PART:\n'+file)
+            else:
+                print('DISK:\n'+file)
+             
+         
+            
