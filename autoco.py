79ac1e07b91ac005b5676a9863a143adce26bce1
Javier Herrera <muchorrollodejavi@hotmail.com>
2017-02-05 14:58:16 -0500

    Ya funciona, aunque con limitaciones, aún faltan cosas como gpio y detectar si montar manualmente el disco o ir a la ruta en la que está montado

--- autoco.py
+++ autoco.py
@@ -13,16 +13,28 @@ def check_action(file):
     actions = {'1':ddcopy,'2':filecopy}
     results = [s for s in re.split(':',file)] #parte la cadena en campos, usa ":" como separador para obtener el puerto del pendrive
     puerto = results.pop(3)
+    print(puerto)
+    if puerto == '1':
+        if not 'part' in file:
+            print('ddcopy')
+            ddcopy(file)
+    elif puerto == '2':
+        print('Filecopy')
+        filecopy(file)
+    print('this is theee eeenndd')
 
 #Realiza la copia de ficheros(usa la partición, no el disco), es mas rápida que el dd pero a costa de copiar menos información
-#Guarda en FC_SAVE_DIR los archivos guardados con el formato especificado en FC_TIME_FORMAT como nombre
-def filecopy():
-	return
+#Guarda en FC_SAVE_DIR los archivos, en una directorio con el formato especificado en FC_TIME_FORMAT como nombre
+def filecopy(file):
+    call(['mount',file,settings.MOUNT_DIR])
+    save_dir = settings.FC_SAVE_DIR+time.strftime(settings.FC_TIME_FORMAT,time.gmtime())
+    call(['mkdir',save_dir])
+    call(['cp','-R',settings.MOUNT_DIR,save_dir])
 
 
 #Realiza la copia binaria del disco entero, es mas lenta pero copia toda la información incluso la borrada y no sobreescrita
 #Guarda en DD_SAVE_DIR la copia binaria en formato iso con el formato especificado en DD_TIME_FORMAT como nombre
-def ddcopy():
+def ddcopy(file):
     call(['dd','if=/dev/disk/by-path/'+file,'of='+settings.DD_SAVE_DIR+time.strftime('%S:%H-%d-%m-%Y',time.gmtime())+'.iso'])
 
 while True:
@@ -31,11 +43,6 @@ while True:
         temp_disk.append(file)
         if disks.count(file) == 0:
             disks.append(file) #guardo en una lista cada uno de los archivos
-            if 'part' in file: #si el archivo es una partición
-                print('PART:\n'+file)
-                check_action(file)
-            else: #si no es una partición, sino el disco completo
-                print('DISK:\n'+file)
-
+            check_action(file)
 
             
--- settings.py
+++ settings.py
@@ -1,1 +1,5 @@
 DD_SAVE_DIR = '~/'
+MOUNT_DIR = '/mnt'
+FC_SAVE_DIR = '/home/endor/'
+FC_TIME_FORMAT = '%S:%H-%d-%m-%Y'
+
