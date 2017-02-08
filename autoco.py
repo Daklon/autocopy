73d9455930fbbba80a4367194c986974842e53b6
Javier Herrera <muchorrollodejavi@hotmail.com>
2017-02-08 12:11:59 -0500

    Añadida versión preliminar del control de leds, añadidas opciones DEBUG para mostrar info por pantalla y DD_TIME_FORMAT Script y Settings configuradas para trabajar con rpi1, no se ha probado con otras versiones de rpi Correcciones varias para hacer el código más legible

--- autoco.py
+++ autoco.py
@@ -1,46 +1,102 @@
-from subprocess import call
+# -*- coding:utf-8 -*-
+import re
 import os
 import glob
-import settings
 import time
-import re
+import settings
+import RPi.GPIO as GPIO
+from subprocess import call
 
-disks = []
-temp_disks = []
+
+
+def on_led(number):
+    GPIO.output(number, GPIO.HIGH)
+
+def off_led(number):
+    GPIO.output(number, GPIO.LOW)
 
 def check_action(file):
+    on_led(17)
     results = [s for s in re.split(':',file)] #parte la cadena en campos, usa ":" como separador para obtener el puerto del pendrive
-    puerto = results.pop(3)
-    print(puerto)
-    if puerto == '1':
-        if not 'part' in file:
-            ddcopy(file)
-    elif puerto == '2':
-        filecopy(file)
+    if DEBUG:
+        print ("comprobando dispositivos: " + str(results))
+    try:
+        puerto = results.pop(POP_INDEX)
+        if puerto == '1.2':
+            if DEBUG:
+                print ("Detectado puerto 1.2")
+            if not 'part' in file:
+                if DEBUG:
+                    print ("ddcopy by 1.2")
+                ddcopy(file)
+        elif puerto == '1.3':
+            if DEBUG:
+                print ("Detectado 1.3")
+            if 'part' in file:
+                if DEBUG:
+                    print ("filecopy by 1.3")
+                filecopy(file)
+    except Exception as e:
+        print("[ERROR] " + str(e.message))
+    off_led(17)
 
 #Realiza la copia de ficheros(usa la partición, no el disco), es mas rápida que el dd pero a costa de copiar menos información
 #Guarda en FC_SAVE_DIR los archivos, en una directorio con el formato especificado en FC_TIME_FORMAT como nombre
 def filecopy(file):
-    call(['mount',file,settings.MOUNT_DIR])
+    on_led(22)
+    call(['umount', file,settings.MOUNT_DIR])
+    call(['mount', file,settings.MOUNT_DIR])
     save_dir = settings.FC_SAVE_DIR+time.strftime(settings.FC_TIME_FORMAT,time.gmtime())
     call(['mkdir',save_dir])
     call(['cp','-R',settings.MOUNT_DIR,save_dir])
-
+    off_led(22)
 
 #Realiza la copia binaria del disco entero, es mas lenta pero copia toda la información incluso la borrada y no sobreescrita
 #Guarda en DD_SAVE_DIR la copia binaria en formato iso con el formato especificado en DD_TIME_FORMAT como nombre
 def ddcopy(file):
-    call(['dd','if=/dev/disk/by-path/'+file,'of='+settings.DD_SAVE_DIR+time.strftime('%S:%H-%d-%m-%Y',time.gmtime())+'.iso'])
+    on_led(22)
+    command = 'dd if=/dev/disk/by-path/' + file + ' of=' + settings.DD_SAVE_DIR + time.strftime(DD_TIME_FORMAT,time.gmtime()) + '.iso'
+    if DEBUG:
+        print("command:" + command)
+    call(command.split(" "))
+    off_led(22)
 
-while True:
-    os.chdir('/dev/disk/by-path') #directorio donde buscar los discos y particiones
+
+def main():
+    disks = []
     temp_disks = []
-    for file in glob.glob('*[usb]*'): #filtro para solo mostrar los dispositivos usb
-        temp_disks.append(file)
-        if disks.count(file) == 0:
-            disks.append(file) #guardo en una lista cada uno de los archivos
-            check_action(file)
-    if len(temp_disks) < len(disks): #compruebo si se ha desenchufado algún disco para eliminarlo de la lista
-        for file in disks:
-            if not file in temp_disks:
-                disks.remove(file)
+
+    # LED CONFIG
+    if DEBUG:
+        print("Configuración de leds")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(17,GPIO.OUT)
+    GPIO.setup(21,GPIO.OUT)
+    GPIO.setup(22,GPIO.OUT)
+    GPIO.setup(23,GPIO.OUT)
+    GPIO.setup(24,GPIO.OUT)
+
+    off_led(17)
+    off_led(21)
+    off_led(22)
+    off_led(23)
+    off_led(24)
+
+    if DEBUG:
+        print("Iniciando autoco.py")
+
+    while True:
+        os.chdir('/dev/disk/by-path') #directorio donde buscar los discos y particiones
+        temp_disks = []
+        for file in glob.glob('*[usb]*'): #filtro para solo mostrar los dispositivos usb
+            temp_disks.append(file)
+            if disks.count(file) == 0:
+                if DEBUG:
+                    print("Procesando:" + str(file))
+                disks.append(file) #guardo en una lista cada uno de los archivos
+                check_action(file)
+        if len(temp_disks) < len(disks): #compruebo si se ha desenchufado algún disco para eliminarlo de la lista
+            for file in disks:
+                if not file in temp_disks:
+                    disks.remove(file)
--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
-DD_SAVE_DIR = '~/'
+DD_SAVE_DIR = 'home/pi/'
+DD_TIME_FORMAT = '%H:%S-%d-%m-%Y'
 MOUNT_DIR = '/mnt'
-FC_SAVE_DIR = '/home/endor/'
+FC_SAVE_DIR = '/home/pi/'
 FC_TIME_FORMAT = '%S:%H-%d-%m-%Y'
-
+POP_INDEX = 1
+DEBUG = 0
